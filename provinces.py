aaf7264600232f1b38dde855187a97e06fdf3463
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-06-16 22:21:00 -0600

    Update provinces.py

--- provinces.py
+++ provinces.py
@@ -1,5 +1,21 @@
 def main():
-    provinces_list = read_list()
-    print(provinces_list)
+    cleaned_provinces_list = create_list("provinces.txt")
+    for province in cleaned_provinces_list:
+        print(cleaned_provinces_list)
+    #
+    # cleaned_provinces_list.pop(0)
+    # print(cleaned_provinces_list)
+    # #
+    # cleaned_provinces_list.pop()
+    # print(cleaned_provinces_list)
 
-main()
+def create_list(filename):
+    cleaned_provinces_list = []
+    provinces_list = open(filename, "rt")
+    for province in provinces_list:
+        cleaned_province = province.strip()
+        cleaned_provinces_list.append(cleaned_province)
+    return cleaned_provinces_list
+
+if __name__ == "__main__":
+    main()
