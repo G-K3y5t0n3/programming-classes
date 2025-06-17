c938ed6241bad914ef71c3a910a3aca3548736bd
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-06-16 23:30:57 -0600

    Update provinces.py

--- provinces.py
+++ provinces.py
@@ -1,20 +1,31 @@
+# from pprint import pprint
+
 def main():
+    alberta_count = 0
     cleaned_provinces_list = create_list("provinces.txt")
-    for province in cleaned_provinces_list:
-        print(cleaned_provinces_list)
+    # for province in cleaned_provinces_list:
+    #     print(province)
+    #   pprint(cleaned_provinces_list)
     #
-    # cleaned_provinces_list.pop(0)
-    # print(cleaned_provinces_list)
-    # #
-    # cleaned_provinces_list.pop()
-    # print(cleaned_provinces_list)
+    cleaned_provinces_list.pop(0)
+    #
+    cleaned_provinces_list.pop()
+    #
+    for provinces in range(len(cleaned_provinces_list)):
+        if cleaned_provinces_list[provinces] == "AB":
+            cleaned_provinces_list[provinces] = "Alberta"
+    #
+    print(cleaned_provinces_list)
+    #
+    alberta_count = cleaned_provinces_list.count("Alberta")
+    print(f'\nAlberta occurs {alberta_count} times in the modified list')
 
 def create_list(filename):
     cleaned_provinces_list = []
-    provinces_list = open(filename, "rt")
-    for province in provinces_list:
-        cleaned_province = province.strip()
-        cleaned_provinces_list.append(cleaned_province)
+    with open(filename, "rt") as provinces_list:
+        for province in provinces_list:
+            cleaned_province = province.strip()
+            cleaned_provinces_list.append(cleaned_province)
     return cleaned_provinces_list
 
 if __name__ == "__main__":
