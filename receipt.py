1627c6403355d3fee850b20a2d75c627efb4856f
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-06-26 22:27:11 -0600

    L9 Prove complete

--- receipt.py
+++ receipt.py
@@ -1,6 +1,12 @@
-# def main():
-#     products_dict = read_dictionary()
-#     pass
+def main():
+    products_dict = read_dictionary("products.csv")
+    print(products_dict)
+    print('\nRequested Items')
+    with open("request.csv", "rt") as requests:
+        for i, info in enumerate(requests):
+            if i == 0: continue
+            productID, product_amount = info.strip().split(",")
+            print(f'{products_dict[productID][0]}: {product_amount} @ {products_dict[productID][1]}')
 
 def read_dictionary(filename):
     """Read the contents of a CSV file into a compound
@@ -16,8 +22,18 @@ def read_dictionary(filename):
         for i, info in enumerate(file_info):
             if i == 0: continue
             pNumber, pName, pPrice = info.strip().split(",")
-            products_dict[pNumber] = pPrice
+            products_dict[pNumber] = (pName, pPrice)
     return products_dict
 
-products_dict = read_dictionary("products.csv")
-print(products_dict)
+# def create_request_list(filename):
+#     requests = []
+#     with open(filename, "rt") as request_info:
+#         for i, info in enumerate(request_info):
+#             if i == 0: continue
+#             requests.append(info.strip().split(","))
+#     return requests
+
+
+
+if __name__ == "__main__":
+    main()
