74c0f03cd46fa46070a4d8034279d2b4fd433d1c
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-06-27 19:22:13 -0600

    L10 Prove progress

--- receipt.py
+++ receipt.py
@@ -15,10 +15,14 @@ def main():
     with open("request.csv", "rt") as requests:
         for i, info in enumerate(requests):
             if i == 0: continue
+            # try:
             productID, product_amount = info.strip().split(",")
             total_product_amount += int(product_amount)
             subtotal += (float(products_dict[productID][1]) * float(product_amount))
             print(f'{products_dict[productID][0]}: {product_amount} @ {products_dict[productID][1]}')
+#             except KeyError as key_error:
+#                 print(f'''Error: unknown product ID in the request.csv file
+# {key_error}''')
     sales_tax = subtotal * 0.06
     total = subtotal + sales_tax
     print(f'''\nNumber of Items: {total_product_amount}
@@ -39,12 +43,18 @@ def read_dictionary(filename):
         the contents of the CSV file.
     """
     products_dict = {}
+    # try:
     with open(filename, "rt") as file_info:
         for i, info in enumerate(file_info):
             if i == 0: continue
             pNumber, pName, pPrice = info.strip().split(",")
             products_dict[pNumber] = (pName, pPrice)
     return products_dict
+    # except FileNotFoundError as file_not_found_error:
+    #         print()
+    #         print("Error: Missing file")
+    #         print(type(file_not_found_error).__name__, file_not_found_error, sep=": ")
+           
 
 # def create_request_list(filename):
 #     requests = []
