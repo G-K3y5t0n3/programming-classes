2d734890d23822e0be45cf0081b8a1fd86c396fb
G-K3y5t0n3 <Keystone102825@gmail.com>
2025-07-17 12:39:07 -0600

    Update hr_to_rect_area.py

--- hr_to_rect_area.py
+++ hr_to_rect_area.py
@@ -1,5 +1,3 @@
-# Copyright 2020, Brigham Young University-Idaho. All rights reserved.
-
 import tkinter as tk
 from tkinter import Frame, Label, Button
 from number_entry import IntEntry
@@ -13,7 +11,7 @@ def main():
     # a window is also called a frame.
     frm_main = Frame(root)
     frm_main.master.title("Rectangle Area Calculator")
-    frm_main.pack(padx=41, pady=3, fill=tk.BOTH, expand=1)
+    frm_main.pack(padx=41, pady=31, fill=tk.BOTH, expand=1)
 
     # Call the populate_main_window function, which will add
     # labels, text entry boxes, and buttons to the main window.
@@ -47,40 +45,51 @@ def populate_main_window(frm_main):
         frm_main: the main frame (window)
     Return: nothing
     """
-    # Create a label that displays "Age:"
+    # Create a label that displays "Width:"
     lbl_width = Label(frm_main, text="Width:")
+
+    # Create a label that displays "Height"
     lbl_height = Label(frm_main, text="Height:")
+
+    # Create a label that displays a warning to not use negative integers
     lbl_warning = Label(frm_main, text="Do NOT use Negative Integers (-1, -2, etc)")
 
     # Create an integer entry box where the user will enter her age.
-    ent_width = IntEntry(frm_main, width=4, lower_bound=12, upper_bound=90)
+    ent_width = IntEntry(frm_main, width=4, lower_bound=0,)
 
-    # Create a label that displays "years"
-    lbl_age_units = Label(frm_main, text="years")
+    # Create an integer entry box where the user will enter her age.
+    ent_height = IntEntry(frm_main, width=4, lower_bound=0,)
 
-    # Create a label that displays "Rates:"
-    lbl_rates = Label(frm_main, text="Area:")
+    # Create a label that displays "units"
+    lbl_width_units = Label(frm_main, text="units")
+    lbl_height_units = Label(frm_main, text="units")
+
+
+    # Create a label that displays "Area:"
+    lbl_area = Label(frm_main, text="Area:")
 
     # Create labels that will display the results.
-    lbl_slow = Label(frm_main, width=3)
-    lbl_fast = Label(frm_main, width=3)
-    lbl_rate_units = Label(frm_main, text="beats/minute")
+    lbl_calculated_area = Label(frm_main, width=3)
+    lbl_calculated_area_units = Label(frm_main, text="units")
 
     # Create the Clear button.
     btn_clear = Button(frm_main, text="Clear")
 
     # Layout all the labels, entry boxes, and buttons in a grid.
-    lbl_width.grid(      row=0, column=0, padx=3, pady=3)
-    lbl_height.grid(      row=0, column=0, padx=3, pady=3)
-    ent_width.grid(      row=0, column=1, padx=3, pady=3)
-    lbl_age_units.grid(row=0, column=2, padx=0, pady=3)
-
-    lbl_rates.grid(     row=1, column=0, padx=(30,3), pady=3)
-    lbl_slow.grid(      row=1, column=1, padx=3, pady=3)
-    lbl_fast.grid(      row=1, column=2, padx=3, pady=3)
-    lbl_rate_units.grid(row=1, column=3, padx=0, pady=3)
-
-    btn_clear.grid(row=2, column=0, padx=3, pady=3, columnspan=4, sticky="w")
+    lbl_width.grid(row=0, column=0, padx=3, pady=3)
+    ent_width.grid(row=0, column=1, padx=3, pady=3)
+    lbl_width_units.grid(row=0, column=2, padx=0, pady=3)
+    #
+    lbl_height.grid(row=1, column=0, padx=3, pady=3)
+    ent_height.grid(row=1, column=1, padx=3, pady=3)
+    lbl_height_units.grid(row=1, column=2, padx=0, pady=3)
+    #
+    #
+    lbl_area.grid(row=2, column=0, padx=3, pady=3)
+    lbl_calculated_area.grid(row=2, column=1, padx=3, pady=3)    
+    lbl_calculated_area_units.grid(row=2, column=2, padx=0, pady=3)
+    #
+    btn_clear.grid(row=3, column=0, padx=3, pady=3, columnspan=4, sticky="w")
 
 
     # This function will be called each time the user releases a key.
@@ -89,28 +98,22 @@ def populate_main_window(frm_main):
         and fastest beneficial heart rates.
         """
         try:
-            # Get the user's age.
-            age = ent_width.get()
+            # Get the user's intended width.
+            width = ent_width.get()
 
-            # Compute the user's maximum heart rate.
-            max_rate = 220 - age
+            # Get the user's intended height.
+            height = ent_height.get()
 
-            # Compute the user's slowest and
-            # fastest beneficial heart rates.
-            slow = max_rate * 0.65
-            fast = max_rate * 0.85
+            # Compute the user's area from the user's width and height.
+            area = width * height
 
-            # Display the slowest and fastest benficial
-            # heart rates for the user to see.
-            lbl_slow.config(text=f"{slow:.0f}")
-            lbl_fast.config(text=f"{fast:.0f}")
+            # Display the calculated area
+            lbl_calculated_area.config(text=f"{area:.0f}")
 
         except ValueError:
             # When the user deletes all the digits in the age
             # entry box, clear the slowest and fastest labels.
-            lbl_slow.config(text="")
-            lbl_fast.config(text="")
-
+            lbl_calculated_area.config(text="")
 
     # This function will be called each time
     # the user presses the "Clear" button.
@@ -118,8 +121,8 @@ def populate_main_window(frm_main):
         """Clear all the inputs and outputs."""
         btn_clear.focus()
         ent_width.clear()
-        lbl_slow.config(text="")
-        lbl_fast.config(text="")
+        ent_height.clear()
+        lbl_calculated_area.config(text="")
         ent_width.focus()
 
     # Bind the calculate function to the age entry box so
